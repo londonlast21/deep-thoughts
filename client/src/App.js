9a693f96676ff59af5dfcc37363f5eb32f4355f3
jack <biffisbuffm@gmail.com>
2020-10-15 19:06:01 -0500

    works rn

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,26 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+
+// add these two library import statements
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
