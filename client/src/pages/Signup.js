d48b8fd58f22e706db0642c08ff599805b43ed28
jack <biffisbuffm@gmail.com>
2020-10-16 19:22:25 -0500

    login

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import Auth from '../utils/auth';
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  
   const [addUser, { error }] = useMutation(ADD_USER);
   // update state based on form input changes
   const handleChange = (event) => {
@@ -25,7 +27,7 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState }
       });
-      console.log(data);
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
